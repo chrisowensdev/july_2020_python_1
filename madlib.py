881ecb4c0e3c448fb8764351c2223a6ded6404c0
Chris Owens <guitarrockstarco@gmail.com>
2020-07-28 18:57:26 -0400

    Created madlib.py

---
+++ madlib.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+subject = input('What is your favorite subject? ')
+print('%s\'s favorite subject in school is %s' % (name, subject))
+print('What is name? %s' % (name))
+print('What is subject? %s' % (subject))
+print('%s\'s favorite subject in school is %s' % (name, subject))
