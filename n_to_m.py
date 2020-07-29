ee129f0aaf2469d05d5347bad65583f4a44b5418
Chris Owens <guitarrockstarco@gmail.com>
2020-07-29 15:00:55 -0400

    fixed n_to_m.py with <=

--- n_to_m.py
+++ n_to_m.py
@@ -2,6 +2,6 @@ n = int(input("Enter number to start? "))
 m = int(input("Enter number to end? "))
 print("Start from: " + str(n))
 print("End from: " + str(m))
-while n < m + 1:
+while n <= m:
     print(n)
     n += 1
