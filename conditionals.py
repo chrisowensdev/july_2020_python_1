1571e2631df498dcae527b7cb4f27c06ddc64c31
Chris Owens <guitarrockstarco@gmail.com>
2020-07-29 12:11:40 -0400

    Add 'or' to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,11 +1,18 @@
 user_input = int(input("Guess a number 5-50: "))
 magic_number = 6
 
-if user_input == magic_number:
-    print("ARE YOU A MIND READER!?!?")
-elif user_input > 50:
-    print("Guess was too high!")
-elif user_input < 5:
-    print("Guess was too low.")
+# if user_input == magic_number:
+#     print("ARE YOU A MIND READER!?!?")
+# elif user_input > 50:
+#     print("Guess was too high!")
+# elif user_input < 5:
+#     print("Guess was too low.")
+# else:
+#     print("Sorry. Try again.")
+
+if user_input == magin_number:
+    print("ARE YOU A MINDREADER!?!?!")
+elif (user < 5) or (user_input > 50):
+    print("YOU'RE OUT OF RANGE!")
 else:
     print("Sorry. Try again.")
