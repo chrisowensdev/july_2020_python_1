adf0cabde6827394c63f04f4dfa04aeea65c999b
Chris Owens <guitarrockstarco@gmail.com>
2020-07-29 11:39:11 -0400

    Added elif to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,11 @@
-user_input = int(input("Guess a number: "))
+user_input = int(input("Guess a number 5-50: "))
 magic_number = 6
 
 if user_input == magic_number:
-  print("ARE YOU A MIND READER!?!?")
+    print("ARE YOU A MIND READER!?!?")
+elif user_input > 50:
+    print("Guess was too high!")
+elif user_input < 5:
+    print("Guess was too low.")
 else:
-  print("Sorry. Try again.")
+    print("Sorry. Try again.")
