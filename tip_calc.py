4b5201acf254f1497ab821df8ce3b6227a95e63e
Chris Owens <guitarrockstarco@gmail.com>
2020-07-29 19:07:16 -0400

    Create tip_calc.py

---
+++ tip_calc.py
@@ -0,0 +1,22 @@
+bill_amount = int(input("Total bill amount? "))
+level_of_service = input("Level of service? ")
+lower_los = level_of_service.lower()
+tip = 0
+
+# Good -> 20%
+# Fair -> 15%
+# Bad -> 10%
+
+if lower_los == 'good':
+    tip = .20
+elif lower_los == 'fail':
+    tip = .15
+elif lower_los == 'bad':
+    tip = .10
+else:
+    print('Invalid Input')
+
+total = (bill_amount * tip) + bill_amount
+
+print("Tip amount: $%.2f" % (tip * 100))
+print("Total amount: $%.2f" % total)
