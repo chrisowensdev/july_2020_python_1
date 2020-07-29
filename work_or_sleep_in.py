3b7fb7cc4f9ccdc7cae82b7f6e33e3ad3a9947c6
Chris Owens <guitarrockstarco@gmail.com>
2020-07-29 11:51:56 -0400

    Created work_or_sleep_in.py

---
+++ work_or_sleep_in.py
@@ -0,0 +1,9 @@
+day = int(input('Day (0-6)? '))
+if day == 0:
+    print('Sleep in')
+elif day == 6:
+    print('Sleep in')
+elif day > 6:
+    print('Invalid Input')
+else:
+    print('Go to work')
