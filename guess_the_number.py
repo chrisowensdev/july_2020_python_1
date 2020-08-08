49019ed60aeb7d8de87e3cb4de2935b7c805a366
Chris Owens <guitarrockstarco@gmail.com>
2020-08-08 12:08:29 -0400

    Limit guesses to 3

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,14 +4,20 @@ print("I am thinking of a number between 1 and 10.\n")
 
 random_number = random.randint(1, 10)
 
+count = 1
 while True:
     user_input = int(input("Whats the numnber? "))
     if user_input == random_number:
         print("You win!")
         break
+    elif count == 3:
+        print("Too many guesses")
+        break
     elif user_input > random_number:
         print("%d is too high." % user_input)
+        count += 1
     elif user_input < random_number:
-        print("%d is too low." % user_input1)
+        print("%d is too low." % user_input)
+        count += 1
     else:
-        print("Nope, try again")
+        print("Nope try again")
